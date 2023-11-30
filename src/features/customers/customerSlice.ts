b0ede8279bae7b6bcbe042762cbb650613d77bc3
Aurelia Robu <robuaurelia@gmail.com>
2023-11-30 10:14:45 +0200

    customer store with RTK

--- src/features/customers/customerSlice.ts
+++ src/features/customers/customerSlice.ts
@@ -1,4 +1,4 @@
-import { Action, CustomerPayload } from '../../interfaces/commonInterfaces';
+import { createSlice } from '@reduxjs/toolkit';
 
 interface CustomerState {
   fullName: string;
@@ -6,11 +6,42 @@ interface CustomerState {
   createdAt: string;
 }
 
-const initialStateCustomer: CustomerState = {
+const initialState: CustomerState = {
   fullName: '',
   nationalId: '',
   createdAt: '',
 };
+
+const customerSlice = createSlice({
+  name: 'customer',
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalId) {
+        return {
+          payload: {
+            fullName,
+            nationalId,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalId = action.payload.nationalId;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export const { createCustomer, updateName } = customerSlice.actions;
+export default customerSlice.reducer;
+
+/*
 
 export default function customerReducer(
   // eslint-disable-next-line @typescript-eslint/default-param-last
@@ -50,3 +81,5 @@ export function updateName(fullName: string) {
     payload: fullName,
   };
 }
+
+ */
